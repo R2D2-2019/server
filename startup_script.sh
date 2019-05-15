f32ee8be4d19667317976b234d1d6c8f48753cfd
CoreOS Admin <core@dhcp-145-89-224-204.medewerkers.ad.hvu.nl>
2019-05-15 13:52:56 +0000

    Startup portainer whenever the server starts ~ Nathan Hoekstra

---
+++ startup_script.sh
@@ -0,0 +1,5 @@
+# R2D2 Startup script
+#!/bin/bash
+
+# Run Portainer container
+docker run -d -p 9000:9000 --name portainer --restart always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
