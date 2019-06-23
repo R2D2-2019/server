002bff8753a4a091412ff49ed0b9c1dab28ba30e
Florian Humblot <florian.humblot@student.hu.nl>
2019-06-23 14:01:18 +0200

    Formatted the files so it becomes more readable

--- module_images/base_image/docker_tests.sh
+++ module_images/base_image/docker_tests.sh
@@ -1,22 +1,39 @@
 #! /bin/bash
+# If anything goes wrong, exit.
+# if we use 'false' on it's own (without quotes), the program will stop.
+
 set -eo pipefail
+# echoes the command and it's arguments to STDERR
 yell() { echo "$0: $*" >&2; }
-die() { yell "$*"; exit 111; }
+# Calls yell and exits the program using code 111
+die() {
+    yell "$*"
+    exit 111
+}
+# Tries the given command, if it fails, it will run the code on the right hand of the boolean "OR" (||) which calls the 'die' function with all the specified arguments
 try() { "$@" || die "cannot $*"; }
+# Special case for try to make the missing module more readable.
 try_import() { "$@" || die "cannot find module ${@: -1}, full command: $*"; }
 
+# Specify all the files you expect to exist here. Syntax is space separated double quoted values.
 declare -a files_to_test=("main.py" "requirements.txt" "start_module.sh")
+# Specify all the directories you expect to exist here. Syntax is space separated double quoted values.
 declare -a directories_to_test=("module")
-readarray libs < requirements.txt
+# reads the requirements.txt file of the module and puts each line in an array element
+readarray libs <requirements.txt
 
 for file in ${files_to_test[@]}; do
-    try [ -e $file ]                    # check if the file exists
+    # check if the file exists
+    try [ -e $file ]
 done
 
 for dir in ${directories_to_test[@]}; do
-    try [ -d $dir ]                     # check if the directory exists
+    # check if the directory exists
+    try [ -d $dir ]
 done
 
 for lib in ${libs[@]}; do
+    printf $'%-30s %-15s is installed\n' "checking that library" "$lib"
+    # check if all libraries that are specified in requirements.txt are installed.
     try_import python -c "import sys, pkgutil; sys.exit(0 if pkgutil.find_loader(sys.argv[1]) else 1)" $lib
 done
--- module_images/dockerfile_tests.sh
+++ module_images/dockerfile_tests.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # If anything returns an error, just stop the tests.
+# if we use 'false' on it's own (without quotes), the program will stop.
 set -eo pipefail
 
 # If debug is set, we output all the commands and variables being run
@@ -7,46 +8,66 @@ set -eo pipefail
 
 # CD to the directory of the script, so that we know where the image folders are.
 cd "$(dirname "$0")"
-> build.log
+#clears the build.log file
+>build.log
+# echoes the command and it's arguments to STDERR
 yell() { echo "$0: $*" >&2; }
-die() { yell "$*"; exit 111; }
+# Calls yell and exits the program using code 111
+die() {
+    yell "$*"
+    exit 111
+}
+# Tries the given command, if it fails, it will run the code on the right hand of the boolean "OR" (||) which calls the 'die' function with all the specified arguments
 try() { "$@" || die "cannot $*"; }
 
+# array of image names. These should be the image directories.
+# Space separated double quoted values.
 declare -a images_to_test=("base_image" "main_server" "microphone" "swarm_analytics" "swarm_management" "swarm_ui" "CAN_Navigation" "voice_interaction")
-declare images_exist=true
 for i in ${images_to_test[@]}; do
-    name=${i,,}                                     # Docker tags should be lower_case
-    if ! docker inspect "$name" &>/dev/null; then   # redirect STDOUT to /dev/null to only show errors.
+    # Docker tags should be lower_case
+    name=${i,,}
+    # redirect STDOUT to /dev/null to only show errors.
+    if ! docker inspect "$name" &>/dev/null; then
         printf '%-30s image does not exist\n' "$i"
-        images_exist=false
     else
         printf '%-30s image exists, cleaning it.\n' "$i"
+        # remove the image if it exists so that we can actually test a clean build.
         docker image rm -f "$name"
     fi
 done
 
 for x in ${images_to_test[@]}; do
+    # docker tags have to be lower_case
     name=${x,,}
-    printf $'%-30s %-30s\n' "$name" "Building image"    # docker tags have to be lower_case
-    docker build -t "$name" ./"$x" &>build.log &        # redirect STDOUT to /dev/null to only show errors. Also run in background
-    pid=$!                                              # Process Id of the previous running command
-    spin='-\|/'                                         # spinner characters
-    i=0                                                 # increment the character we want to show
-    while kill -0 $pid 2>/dev/null; do                  # wait for the docker build to be complete
-        i=$(((i + 1) % 4))                              # get the animation character
-        printf "\r${spin:$i:1}"                         # erase the previous animation character and print the next one.
-        sleep .1                                        # delay a slight bit.
+    printf $'%-30s %-30s\n' "$name" "Building image"
+    # redirect STDOUT to /dev/null to only show errors. Also run in background
+    docker build -t "$name" ./"$x" &>build.log &
+    # Process Id of the previous running command
+    pid=$!
+    # spinner characters
+    spin='-\|/'
+    # increment the character we want to show
+    i=0
+    # wait for the docker build to be complete
+    while kill -0 $pid 2>/dev/null; do
+        # get the animation character
+        i=$(((i + 1) % 4))
+        # erase the previous animation character and print the next one.
+        printf "\r${spin:$i:1}"
+        # delay a slight bit.
+        sleep .1
     done
+    # for some reason, if the image build fails it doesn't actually stop the script, so we check that the image exists in docker.
+    # The image should only exist if the build was successful.
     if ! docker inspect "$name" &>/dev/null; then
         printf $'\r Something went wrong while building the image! check the logs!\n'
         false
     else
-        printf $'\r%-30s image built.\n' "$x"               # print that the current image is built so that it doesn't feel so long
+        printf $'\r%-30s image built.\n' "$x" # print that the current image is built so that it doesn't feel so long
     fi
+    # runs the image-specific test script.
     if ! docker run -ti -e="CLI=true" $name ./docker_tests.sh; then
         false
     fi
-    echo "test"
     break
 done
-
