fce1560d49e9567012dc5032c904353a59061cd7
Florian Humblot <florian.humblot@student.hu.nl>
2019-06-23 09:19:55 +0200

    Added a CLI entrypoint for running commands using the  command. This way we'll be able to test certain things per image without having to modify them any further.

--- module_images/voice_interaction/start_module.sh
+++ module_images/voice_interaction/start_module.sh
@@ -1,4 +1,11 @@
 #! /bin/sh
+COMMAND="$@"
+
+if [ -n "$CLI" ] && [ $CLI = true ]; then
+    COMMAND="$@"
+fi
+
+exec $COMMAND
 
 # Run the main.py and redirect it's output to the module.log file.
 python3 main.py 2>&1 | tee module.log &
