28e34bc0e1ca9ec242259ed64b0d0cb030ef9309
Florian Humblot <florian.humblot@student.hu.nl>
2019-06-23 14:07:35 +0200

    Updated the start_module script for all the modules.

--- module_images/microphone/start_module.sh
+++ module_images/microphone/start_module.sh
@@ -1,15 +1,15 @@
 #! /bin/sh
-COMMAND="$@"
+# Read the input arguments of the entrypoing.
+COMMAND="$@" 
 
+# If the "CLI" environment variable is set to true, we cun the command.
 if [ -n "$CLI" ] && [ $CLI = true ]; then
     COMMAND="$@"
+    exec $COMMAND
 fi
-
-exec $COMMAND
 
 # Run the main.py and redirect it's output to the module.log file.
 python3 main.py 2>&1 | tee module.log &
-while true
-do
-	sleep 1
+while true; do
+    sleep 1
 done
